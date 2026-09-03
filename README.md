4e9e179515adf9a632b717771d04d6b01c483327
sayali-chougule <sayalichougule2269@gmail.com>
2026-09-03 13:18:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Extract temperature reading with get_temp_api
+# Append reading to temperature log
+# Buffer last hour of readings
+# Call get_stats.py to aggregate the readings
+# Load the stats using load_stats_api
---
+++ Temperature_ETL.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# Extract temperature reading with get_temp_api
+# Append reading to temperature log
+# Buffer last hour of readings
+# Call get_stats.py to aggregate the readings
+# Load the stats using load_stats_api
