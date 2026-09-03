f972da44e894368bde1c6c21f245eb67d2268f15
sayali-chougule <sayalichougule2269@gmail.com>
2026-09-03 13:30:11 -0400

    etl pipeline

---
+++ get_stats.py
@@ -0,0 +1,10 @@
+# Reads temperature from log files
+# Calculates temperature stats
+# Writes temperature stats to file
+# Input/output filenames specified as command line arguements
+
+# Call get_stats.py to aggregate the readings
+# $ python3 get_stats.py temperature.log temp_stats.csv
+
+# Load the stats using load_stats_api
+# $ load_stats_api temp_stats.csv
